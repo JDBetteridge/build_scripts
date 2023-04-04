f86ca7d1db4fff191dc397276ec312eb03e1413f
Jack Betteridge <j.betteridge@imperial.ac.uk>
2023-04-04 23:57:29 +0100

    Update python builder

--- python_build.sh
+++ python_build.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 # Initially need to:
 # git clone git@github.com:python/cpython.git
+# cd cpython
 # git fetch --all
-# for VERSION in v3.7.12 v3.8.12 v3.9.7 v3.10.0 v3.10.8 v3.11.0
+# for VERSION in v3.8.16 v3.9.16 v3.10.10 v3.11.2
 # do
 #     git checkout -b $VERSION tags/$VERSION
 # done
+# sudo mkdir -p /opt/python
+# sudo chown jack /opt/python
 
 export INSTALL_PATH=/opt/python
 
 PYTHON_DIR=/home/jack/build/cpython
 cd $PYTHON_DIR
 
-for VERSION in v3.7.12 v3.8.12 v3.9.7 v3.10.0 v3.10.8 v3.11.0
+for VERSION in v3.8.16 v3.9.16 v3.10.10 v3.11.2
 do
     git checkout $VERSION
     git clean -fdx
 
     PY_LDFLAGS="-Wl,-rpath=$INSTALL_PATH/$VERSION/lib -L$INSTALL_PATH/$VERSION/lib"
-    if [ $VERSION != v3.7.12 ]; then
-		OPT="--enable-optimizations"
-	else
-		# Python 3.7 optimisation tests take too long/hang
-		OPT=""
-	fi
+    OPT="--enable-optimizations"
     ./configure --prefix=$INSTALL_PATH/$VERSION \
         --with-ensurepip \
         --enable-shared \
         $OPT \
         LDFLAGS="$PY_LDFLAGS"
-    make -j16
-    make -j16 install
+    make -j32
+    make -j32 install
 done
 
 git checkout main
