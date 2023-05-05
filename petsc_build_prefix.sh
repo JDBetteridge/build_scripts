5b2fc0806ed91d49ef88ed5197cfc00177eb114e
JDBetteridge <j.betteridge@imperial.ac.uk>
2023-05-05 15:49:33 +0100

    Build scripts for PETSc with 1 set of external deps

---
+++ petsc_build_prefix.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# INSTALL_DIR=/opt/petsc
+# See ./mybuilds/configure.py for detailed options
+# Initially need to:
+# curl -OL https://github.com/eigenteam/eigen-git-mirror/archive/3.3.3.tar.gz
+# mv 3.3.3.tar.gz eigen-3.3.3.tgz
+#
+# # git clone git@gitlab.com:petsc/petsc.git
+# # or
+# git clone https://gitlab.com/petsc/petsc.git upstream-petsc
+# cd upstream-petsc
+# git fetch --all
+# cd ..
+#
+# git clone git@github.com:firedrakeproject/petsc.git firedrake-petsc
+# cd firedrake-petsc
+# git fetch --all
+# cd ..
+#
+# mkdir -p upstream-petsc/my_builds firedrake-petsc/my_builds
+# cp build_scripts/petsc_configure_prefix.py upstream-petsc/my_builds/configure_prefix.py
+# cp build_scripts/petsc_configure_prefix.py firedrake-petsc/my_builds/configure_prefix.py
+# sudo mkdir -p /opt/petsc
+# sudo chown jack /opt/petsc
+# mkdir /opt/petsc/upstream
+# mkdir /opt/petsc/firedrake
+
+## mkdir -p /share/upstream-petsc /share/firedrake-petsc
+## /etc/fstab:
+## # PETSc source
+## /home/jack/build/upstream-petsc   /share/upstream-petsc   none  bind 0 0
+## /home/jack/build/firedrake-petsc  /share/firedrake-petsc  none  bind 0 0
+## sudo systemctl daemon-reload
+## ln -s /share/upstream-petsc/src /opt/petsc/upstream/src
+## ln -s /share/upstream-petsc/include /opt/petsc/upstream/include
+## ln -s /share/firedrake-petsc/src /opt/petsc/firedrake/src
+## ln -s /share/firedrake-petsc/include /opt/petsc/firedrake/include
+
+
+BASE=/home/jack/build
+INSTALL=/opt/petsc
+
+##################
+# PETSc upstream #
+##################
+REMOTE=upstream
+PETSC_DIR=$BASE/${REMOTE}-petsc
+cd $PETSC_DIR
+
+# Cleanup
+rm -rf $INSTALL/$REMOTE/vanilla-*
+rm -rf $PETSC_DIR/vanilla-*
+
+# rebuild
+git fetch --all
+git checkout main
+git pull
+for PETSC_ARCH in vanilla-debug vanilla-opt
+do
+    my_builds/configure_prefix.py \
+		--force $PETSC_ARCH \
+		--prefix=$INSTALL/$REMOTE/$PETSC_ARCH
+    make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
+    make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH install
+done
+
+##########################
+# PETSc Firedrake branch #
+##########################
+REMOTE=firedrake
+PETSC_DIR=$BASE/${REMOTE}-petsc
+cd $PETSC_DIR
+
+# Cleanup
+rm -rf \
+    $INSTALL/$REMOTE/packages \
+    $INSTALL/$REMOTE/minimal-* \
+    $INSTALL/$REMOTE/full-* \
+    $INSTALL/$REMOTE/complex-*
+rm -rf \
+    $PETSC_DIR/packages \
+    $PETSC_DIR/minimal-* \
+    $PETSC_DIR/full-* \
+    $PETSC_DIR/complex-*
+
+# --show-petsc-configure-options --minimal-petsc {0} --complex --petsc-int-type=int64
+export EIGEN_TGZ=$BASE/eigen-3.3.3.tgz
+git checkout firedrake
+git pull
+
+# Build all external packages first
+PETSC_ARCH=packages
+my_builds/configure_prefix.py \
+    --force $PETSC_ARCH \
+    --download-eigen=$EIGEN_TGZ \
+    --prefix=$INSTALL/$REMOTE/$PETSC_ARCH
+make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all-local
+make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH install
+# Criple the install configuration so it isn't mistaken for another PETSc
+mv $PETSC_ARCH/include/petscconf.h $PETSC_ARCH/include/old_petscconf.nope
+mv $INSTALL/$REMOTE/$PETSC_ARCH/include/petscconf.h $PETSC_ARCH/include/old_petscconf.nope
+
+# Build all the different PETSc configurations
+for BUILD in debug opt
+do
+    for PETSC_ARCH in minimal full complex # int64
+    do
+        my_builds/configure_prefix.py \
+			--force $PETSC_ARCH-$BUILD \
+			--prefix=$INSTALL/$REMOTE/$PETSC_ARCH-$BUILD \
+            --package-dir=$INSTALL/$REMOTE/packages
+        make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH-$BUILD all-local
+        make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH-$BUILD install
+    done
+done
---
+++ petsc_configure_prefix.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+import sys
+import os
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser('Custom wrapper from PETSc configure')
+parser.add_argument(
+    'name',
+    type=str,
+    help='Name of preset custom configuration'
+)
+parser.add_argument(
+    '--package-dir',
+    type=str,
+    default='.',
+    help='Path to an external package installation'
+)
+
+args, unknown = parser.parse_known_args()
+
+base_options = [
+    '--with-bison',
+    '--with-c2html=0',
+    '--with-cxx-dialext=C++11',
+    '--with-flex',
+    '--with-fortran-bindings=0',
+    '--with-mpi-dir=/opt/mpich',
+    '--with-zlib',
+    f'PETSC_ARCH={args.name}',
+  ]
+
+debug = ['--with-debugging=1']
+opt = [
+    '--COPTFLAGS=-O3 -march=native -mtune=native',
+    '--CXXOPTFLAGS=-O3 -march=native -mtune=native',
+    '--FOPTFLAGS=-O3 -march=native -mtune=native',
+    '--with-debugging=0',
+]
+
+# All pacakges
+# Specify --download-eigen=$EIGEN_TGZ outside
+packages = [
+    '--download-chaco',
+    '--download-hdf5',
+    '--download-hwloc',
+    '--download-hypre',
+    '--download-metis',
+    '--download-ml',
+    '--download-mumps',
+    '--download-netcdf',
+    '--download-pastix',
+    '--download-pnetcdf',
+    '--download-ptscotch',
+    '--download-scalapack',
+    '--download-suitesparse',
+    '--download-superlu_dist',
+]
+
+package_dir = args.package_dir
+print(f'{package_dir =}')
+
+# Firedrake
+minimal = [
+    f'--with-chaco-dir={package_dir}',
+    f'--with-eigen-dir={package_dir}',
+    f'--with-hdf5-dir={package_dir}',
+    f'--with-mumps-dir={package_dir}',
+    f'--with-ptscotch-dir={package_dir}',
+    f'--with-scalapack-dir={package_dir}',
+    f'--with-superlu_dist-dir={package_dir}',
+]
+
+minimal_not_complex = [f'--with-hypre-dir={package_dir}']
+
+firedrake = minimal + [
+    f'--with-hwloc-dir={package_dir}',
+    f'--with-metis-dir={package_dir}',
+    f'--with-netcdf-dir={package_dir}',
+    f'--with-pastix-dir={package_dir}',
+    f'--with-pnetcdf-dir={package_dir}',
+    f'--with-suitesparse-dir={package_dir}',
+]
+
+firedrake_not_complex = minimal_not_complex + [f'--with-ml-dir={package_dir}']
+
+firedrake_complex = ['--with-scalar-type=complex']
+
+custom_conf = {
+    'vanilla-debug': debug,
+    'vanilla-opt': opt,
+    'packages': base_options + opt + packages,
+    'minimal-debug': debug + minimal + minimal_not_complex,
+    'minimal-opt': opt + minimal + minimal_not_complex,
+    'full-debug': debug + firedrake + firedrake_not_complex,
+    'full-opt': opt + firedrake + firedrake_not_complex,
+    'complex-minimal-debug': debug + minimal + firedrake_complex,
+    'complex-minimal-opt': opt + minimal + firedrake_complex,
+    'complex-debug': debug + firedrake + firedrake_complex,
+    'complex-opt': opt + firedrake + firedrake_complex,
+    'custom': unknown,
+    'test': unknown
+}
+
+
+if __name__ == '__main__':
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+
+  print('Congiguring install for', base_options[0])
+  configure_options = base_options + custom_conf[args.name]
+  configure.petsc_configure(configure_options)
